6ac40912965ae062ebce76004785e732f7580eda
fabriceds <fabrice.simonin@gmail.com>
2013-10-02 18:31:40 +0200

    Closes #4

--- javascripts/controllers/authController.js
+++ javascripts/controllers/authController.js
@@ -1,10 +1,22 @@
 function authController($scope, $http, $location, Smartgeo, SQLite){
+    var user = JSON.parse(localStorage.user || '{"firstLogin":true,"username":"","pwd":""}');
+    $scope.username = user.username;
+    $scope.pwd = user.pwd;
+    
+
     $scope.login = function(){
         var url  = Smartgeo.get('url')+"global.auth.json";
             url += "&login="+encodeURIComponent($scope.username);
             url += "&pwd="+encodeURIComponent($scope.pwd);
         $http.get(url)
             .success(function(){
+                if(user.firstLogin) {
+                    var savePwd  = confirm("Souhaitez-vous que l'application retienne votre mot de passe ?");
+                    localStorage.user = JSON.stringify({
+                        username: $scope.username,
+                        pwd: savePwd ? $scope.pwd : ''
+                    });
+                }
                 $location.path('sites');
             }).error(function(){
                 window.alert('erreur');
