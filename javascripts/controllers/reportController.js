bcefdfe4baabfb5ae217833585cc6c16ae57632e
gulian <gulian@orange.fr>
2013-10-02 23:25:05 +0200

    les id des objets passés dans l'url sont automatiquement selectionnés dans le formulaire de compte-rendu (reportController)

--- javascripts/controllers/reportController.js
+++ javascripts/controllers/reportController.js
@@ -1,10 +1,19 @@
 function reportController($scope, $routeParams, $window, $rootScope, Smartgeo,  $location, $http){
 
     $scope.site = JSON.parse(localStorage.sites)[$routeParams.site] ;
-
     $scope.report = {
         assets: [],
-        fields: {}
+        fields: {},
+        activity: null
+    };
+
+    $routeParams.assets =  ($routeParams.assets && $routeParams.assets.split(',')) || []  ;
+
+    // Formatage des id de l'url en objet { 'id ' : int } pour pouvoir binder au ng-options du select
+    // et setter des valeurs par defaut
+    // TODO: récupérer les objets entier en base, car on a besoin des attributs pour renseigner les valeurs par defauts
+    for (var i = 0; i < $routeParams.assets.length; i++) {
+         $scope.report.assets.push( { id: $routeParams.assets[i]*1 });
     };
 
     $scope.loadAssets = function(){
@@ -14,37 +23,25 @@ function reportController($scope, $routeParams, $window, $rootScope, Smartgeo,  
         });
     };
 
-    for (var i = 0; i < $scope.site.activities.length; i++) {
-        if($scope.site.activities[i].id == $routeParams.activity) {
-            $scope.report = {
-                fields : {},
-                activity : $scope.site.activities[i]
-            };
-            break;
+    if($routeParams.activity){
+        for (var i = 0; i < $scope.site.activities.length; i++) {
+            if($scope.site.activities[i].id == $routeParams.activity) {
+                $scope.report.activity = $scope.site.activities[i];
+                break;
+            }
         }
-    }
-
-    if($routeParams.assets){
-        $scope.report.assets = $routeParams.assets.split(',');
-    }
-
-    if($scope.report && $scope.report.activity){
         $scope.loadAssets();
     }
 
     $scope.sendReport = function (event) {
         $scope.report.activity = $scope.report.activity.id ;
-        console.log($scope.report);
         $http.post(Smartgeo.get('url')+'gi.maintenance.mobility.report.json', $scope.report)
             .success(function(){
                 $location.path('map/'+$scope.site.id);
-                console.log(arguments);
             })
             .error(function(){
                 $location.path('map/'+$scope.site.id);
                 console.log(arguments);
             });
-
-        return false ;
     };
 }
