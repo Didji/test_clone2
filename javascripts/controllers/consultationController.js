79bf4bade7ba5a658953cdaa59987012afd04247
gulian <gulian@gulian.fr>
2013-10-02 17:44:11 +0200

    detail du patrimoine dans le panneau de consultation

--- javascripts/controllers/consultationController.js
+++ javascripts/controllers/consultationController.js
@@ -1,6 +1,5 @@
-function consultationController($scope){
+function consultationController($scope, $rootScope){
     $scope.state  = 'close';
-
     $scope.$on("UPDATE_CONSULTATION_ASSETS_LIST", function(event, assets){
         $scope.assets = assets;
         $scope.state  = 'open';
@@ -10,17 +9,4 @@ function consultationController($scope){
     $scope.close = function(){
         $scope.state = 'close' ;
     };
-
-    // document.getElementById( 'trigger2' ).addEventListener('click', function( ev ) {
-    //     ev.stopPropagation();
-    //     ev.preventDefault();
-    //     document.getElementById( 'mp-pusher' ).style.WebkitTransform = 'translate3d(-300px,0,0)';
-    //     document.getElementById( 'mp-pusher' ).style.transform = 'translate3d(-300px,0,0)';
-    // });
-    // document.getElementById( 'trigger3' ).addEventListener('click', function( ev ) {
-    //     ev.stopPropagation();
-    //     ev.preventDefault();
-    //     document.getElementById( 'mp-pusher' ).style.WebkitTransform = 'translate3d(0,0,0)';
-    //     document.getElementById( 'mp-pusher' ).style.transform = 'translate3d(0,0,0)';
-    // });
 }
--- javascripts/controllers/menuController.js
+++ javascripts/controllers/menuController.js
@@ -1,4 +1,3 @@
 function menuController($scope, $routeParams, $window, $rootScope, Smartgeo, SQLite){
-    $scope.site = JSON.parse(localStorage.sites)[$routeParams.site] ;
     new mlPushMenu( document.getElementById( 'mp-menu' ), document.getElementById( 'trigger' ),{type : 'cover'});
 }
