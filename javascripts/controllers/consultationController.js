2cfff3bb03af05bf61bc10f5cc0a9541f9416eb1
gulian <gulian@orange.fr>
2013-10-03 15:35:24 +0200

    consultation avec positionement des marqueurs sur le patrimoine

--- javascripts/controllers/consultationController.js
+++ javascripts/controllers/consultationController.js
@@ -1,14 +1,21 @@
 function consultationController($scope, $rootScope){
     $scope.state  = 'close';
+
     $scope.$on("UPDATE_CONSULTATION_ASSETS_LIST", function(event, assets){
+
         $scope.assets = assets;
         $scope.state  = 'open';
         $scope.$apply();
 
+        // Ferme les onglets ouverts
+        // $(".in").collapse('toggle');
         $(".collapse").collapse({
             toggle: false
+        }).on('show.bs.collapse', function () {
+            $rootScope.$broadcast("HIGHLIGHT_ASSET", $scope.assets[this.id.match(/collapse-(.*)/)[1]]);
+        }).on('hide.bs.collapse', function () {
+            $rootScope.$broadcast("UNHIGHLIGHT_ASSET", $scope.assets[this.id.match(/collapse-(.*)/)[1]]);
         });
-
     });
 
     $scope.close = function(){
@@ -19,14 +26,21 @@ function consultationController($scope, $rootScope){
         $rootScope.$broadcast("LOCATE_ASSET", asset);
     };
 
+    $scope.zoomOnAsset = function(asset){
+        $rootScope.$broadcast("ZOOM_ON_ASSET", asset);
+    };
+
     $scope.toggleConsultationPanel = function(){
         $scope.state = $scope.state === 'open' ? 'close' : 'open' ;
     };
 
     $scope.toggleCollapse = function(event){
-        // console.log($(event.currentTarget));
         event.preventDefault();
-        // $(event.currentTarget).collapse('toggle');
     };
+
+    $scope.definedFilter = function(value){
+        console.log(value);
+        return true;
+    }
 
 }
