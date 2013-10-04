c9eb95acb833273765a2af19d96fbd31c118a536
gulian <gulian@orange.fr>
2013-10-04 22:32:24 +0200

    Enable/Disable consultation in menu closes left panel

--- javascripts/controllers/menuController.js
+++ javascripts/controllers/menuController.js
@@ -1,8 +1,14 @@
 function menuController($scope, $routeParams, $window, $rootScope, Smartgeo, SQLite){
-    new mlPushMenu( document.getElementById( 'mp-menu' ), document.getElementById( 'trigger' ),{type : 'cover'});
+
+    $scope.mlPushMenu = new mlPushMenu( document.getElementById( 'mp-menu' ), document.getElementById( 'trigger' ),{type : 'cover'});
+
+    $scope.close = function (){
+        $scope.mlPushMenu._resetMenu();
+    };
 
     $scope.toggleConsultation = function(event){
         event.preventDefault();
         $rootScope.$broadcast("TOGGLE_CONSULTATION");
+        $scope.close();
     };
 }
