ede14097168757e6412eea464a2f5bb7bc81eeff
gulian <gulian@orange.fr>
2013-10-10 14:38:13 +0200

    Add beggining of solution for fixing UI progress during installation

--- javascripts/controllers/siteUninstallController.js
+++ javascripts/controllers/siteUninstallController.js
@@ -1,5 +1,6 @@
 function siteUninstallController($scope, $rootScope, $routeParams, $http, Smartgeo, SQLite, $location, G3ME, Installer) {
     var sites = JSON.parse(Smartgeo.get('sites'));
+
     $rootScope.site = $rootScope.site || sites[$routeParams.site] ;
 
     Installer.uninstallSite($rootScope.site, function(){
