3e26348cf8245e37279a50cdd348ff22e2d02621
gulian <gulian@orange.fr>
2013-10-17 15:25:41 +0200

    Test unit related commit

--- test/controllersSpec.js
+++ test/controllersSpec.js
@@ -3,74 +3,81 @@
 'use strict';
 
 describe('Smartgeomobile controllers', function() {
-// var $rootScope, $location;
     beforeEach(module('smartgeomobile'));
-//   beforeEach(inject(function($injector) {
-//     $rootScope = $injector.get('$rootScope');
-//     $location = $injector.get('$location');
-//   }));
 
-    // describe('AuthController', function(){
+    describe('AuthController', function(){
 
-    //     it('should instanciate view with message', inject(function($controller) {
-    //         var scope = {},
-    //             ctrl = $controller('authController', {$scope:scope});
-    //         expect(scope.logMessage).toBe("Vérification du serveur");
-    //     }));
+        it('should instanciate view with message', inject(function($controller) {
+            var scope = {},
+                ctrl = $controller('authController', {$scope:scope});
+            expect(scope.logMessage).toBe("Vérification du serveur");
+        }));
 
-    //     it('should login', inject(function($controller) {
-    //         var scope = {},
-    //         ctrl = $controller('authController', {$scope:scope});
-    //         expect(scope.login()).not.toBe(false);
-    //     }));
-    // });
-
-    // describe('SiteListController', function(){
-
-    //     it('should login', inject(function($controller) {
-    //         var scope = {},
-    //             ctrl = $controller('siteListController', {$scope:scope});
-    //         expect(true).not.toBe(false);
-    //     }));
-    // });
+    });
 
     describe('IntentController', function(){
 
-        var scope,
-            ctrl,
-            rootScope ;
+        var $scope;
 
         beforeEach(inject(function ($rootScope, $controller) {
-            rootScope = $rootScope.$new();
-            spyOn(rootScope, '$on');
-            rootScope.site = {id:'Smartgeo'};
-            rootScope.$on('$routeChangeStart', function(scope, next, current){
-                console.log('Changing from '+angular.toJson(current)+' to '+angular.toJson(next));
-            });
+            $scope = $rootScope.$new();
+            $rootScope.site = {id:'Smartgeo'};
+        }));
 
-            ctrl = $controller('intentController', { $scope: scope, $rootScope:rootScope, $routeParams : {
-                    report_url_redirect: 'http://google.fr/?[LABEL_INDEXED_FIELDS]',
-                    report_mission:12772,
-                    report_activity:3,
-                    report_target:'45.526944,4.260833',
-                    report_fields:'fields[Intervenant]=GLO%20GLO',
-                    map_target:'45.526944,4.260833',
-                    map_activity:3,
-                    map_marker:true
+        it('should set correctly $rootScope vars for target=xy & marker=true & mission & prefill(label) & redirect+callback(label)', inject(function($controller, $rootScope) {
+
+            $controller('intentController', { $scope: $scope, $rootScope:$rootScope, $routeParams : {
+                    report_url_redirect:    'http://google.fr/?[LABEL_INDEXED_FIELDS]',
+                    report_mission:         '12772',
+                    report_activity:        '3',
+                    report_target:          '45.526944,4.260833',
+                    report_fields:          'fields[Intervenant]=GLO%20GLO',
+                    map_target:             '45.526944,4.260833',
+                    map_activity:           '3',
+                    map_marker:             'true',
+                    controller:             'map'
             }});
+
+            expect($rootScope.report_url_redirect ).toEqual('http://google.fr/?[LABEL_INDEXED_FIELDS]');
+            expect($rootScope.report_mission      ).toEqual('12772');
+            expect($rootScope.report_activity     ).toEqual('3');
+            expect($rootScope.report_target       ).toEqual('45.526944,4.260833');
+            expect($rootScope.report_fields       ).toEqual('fields[Intervenant]=GLO%20GLO');
+
+            expect($rootScope.map_target          ).toEqual(['45.526944','4.260833']);
+            expect($rootScope.map_activity        ).toEqual('3');
+            expect($rootScope.map_marker          ).toEqual(jasmine.any(Object));
+
+            expect($scope.controller              ).toEqual('map');
 
         }));
 
+        it('should set correctly $rootScope vars for target=asset & marker=true & mission & prefill(label) & redirect+callback(label)', inject(function($controller, $rootScope) {
 
-        it('should set $rootScope vars', inject(function($controller, $rootScope) {
-                expect(rootScope.report_url_redirect ).toEqual('http://google.fr/?[LABEL_INDEXED_FIELDS]');
-                expect(rootScope.report_mission      ).toEqual(12772);
-                expect(rootScope.report_activity     ).toEqual(3);
-                expect(rootScope.report_target       ).toEqual('45.526944,4.260833');
-                expect(rootScope.report_fields       ).toEqual('fields[Intervenant]=GLO%20GLO');
-                expect(rootScope.map_target          ).toEqual(['45.526944','4.260833']);
-                expect(rootScope.map_activity        ).toEqual(3);
-                expect(typeof rootScope.map_marker   ).toEqual('object');
+            $controller('intentController', { $scope: $scope, $rootScope:$rootScope, $routeParams : {
+                    report_url_redirect:    'http://google.fr/?[LABEL_INDEXED_FIELDS]',
+                    report_mission:         '12772',
+                    report_activity:        '3',
+                    report_target:          '172227',
+                    report_fields:          'fields[Intervenant]=GLO%20GLO',
+                    map_target:             '172227',
+                    map_activity:           '3',
+                    map_marker:             'false',
+                    controller:             'map'
+            }});
+
+            expect($rootScope.report_url_redirect ).toEqual('http://google.fr/?[LABEL_INDEXED_FIELDS]');
+            expect($rootScope.report_mission      ).toEqual('12772');
+            expect($rootScope.report_activity     ).toEqual('3');
+            expect($rootScope.report_target       ).toEqual('172227');
+            expect($rootScope.report_fields       ).toEqual('fields[Intervenant]=GLO%20GLO');
+
+            expect($rootScope.map_target          ).toEqual('172227');
+            expect($rootScope.map_activity        ).toEqual('3');
+            expect($rootScope.map_marker          ).toEqual('false');
+
+            expect($scope.controller              ).toEqual('map');
+
         }));
     });
 });
