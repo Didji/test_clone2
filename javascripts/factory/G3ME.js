5494885550875c08a6e05350c00091974c6b9bfc
fabriceds <fsimonin@PC69-040.sig.lan>
2013-10-23 19:16:25 +0200

    G3ME optimization.

--- javascripts/factory/G3ME.js
+++ javascripts/factory/G3ME.js
@@ -6,8 +6,9 @@ angular.module('smartgeomobile').factory('G3ME', function(SQLite, Smartgeo, $roo
 
         mapDiv : null,
         mapDivId: null,
+        databases: {},
 
-        benchMe : false,
+        benchMe : true,
         benchmarks: {},
         benchmarkResults: [],
         benchmarkGlobalResultPerSQL:  0,
@@ -306,10 +307,13 @@ angular.module('smartgeomobile').factory('G3ME', function(SQLite, Smartgeo, $roo
                         G3ME.benchStart(this.site.zones[i].database_name);
                     }
                     (function(zone){
-                        SQLite.openDatabase({
-                            name: zone.database_name,
-                            bgType: 1
-                        }).transaction(function(tx) {
+                        if(!G3ME.databases[zone.database_name]) {
+                            G3ME.databases[zone.database_name] = SQLite.openDatabase({
+                                name: zone.database_name,
+                                bgType: 1
+                            });
+                        }
+                        G3ME.databases[zone.database_name].transaction(function(tx) {
                                 tx.executeSql(request, initargs,
                                     function(tx, results) {
                                         var rows = results.rows;
