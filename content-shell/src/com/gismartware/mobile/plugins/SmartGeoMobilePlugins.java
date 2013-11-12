5ed788ea9a8117ce7c2936f2bb95c74b0e99eb5c
hahtreyou <marc.beudin@gmail.com>
2013-11-12 15:12:54 +0100

    add logs on plugins

--- content-shell/src/com/gismartware/mobile/plugins/SmartGeoMobilePlugins.java
+++ content-shell/src/com/gismartware/mobile/plugins/SmartGeoMobilePlugins.java
@@ -12,6 +12,8 @@ import android.util.Log;
 import com.gismartware.mobile.ActivityCode;
 
 public class SmartGeoMobilePlugins {
+	
+	private static final String TAG = "GimapMobilePlugins";
 
 	private Context context;
 
@@ -21,6 +23,7 @@ public class SmartGeoMobilePlugins {
 	
 	@JavascriptInterface
 	public void launchCamera(int callbackId) {
+		Log.d(TAG, "Request camera");
 		Activity act = (Activity)context;
 		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 		act.startActivityForResult(intent, ActivityCode.CAPTURE_IMAGE.getCode());
@@ -30,6 +33,8 @@ public class SmartGeoMobilePlugins {
 	public void goTo(float longOrig, float latOrig, float longDest, float latDest) {
 		String to = "https://maps.google.com/maps?saddr=" + latOrig + "," + longOrig + "&daddr=" + latDest + "," + longDest;
 		
+		Log.d(TAG, "Goto " + to);
+		
 		Activity act = (Activity)context;
 		Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(to)).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
 		act.startActivity(intent);
@@ -37,7 +42,8 @@ public class SmartGeoMobilePlugins {
 	
 	@JavascriptInterface
 	public void redirect(String url) {
-		Log.d("GimapMobile", "Redirect to URL " + url);
+		Log.d(TAG, "Redirect to URL " + url);
+		
 		Intent intent = new Intent(android.content.Intent.ACTION_VIEW,  Uri.parse(url)).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
 		Activity act = (Activity)context;
 		act.startActivity(intent);
@@ -45,6 +51,8 @@ public class SmartGeoMobilePlugins {
 	
 	@JavascriptInterface
 	public void locate() {
+		Log.d(TAG, "Request location");
+		
 		Activity act = (Activity)context;
 		Intent intent = new Intent(context, GeoLocation.class);
 		act.startActivityForResult(intent, ActivityCode.GEOLOCATE.getCode());
