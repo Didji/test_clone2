09ae2b41aa0e5430122748a09e5c682860447da2
hahtreyou <hahtreyou@users.noreply.github.com>
2013-11-15 15:01:51 +0100

    Update smartgeomobile_en.js

--- javascripts/i18n/smartgeomobile_en.js
+++ javascripts/i18n/smartgeomobile_en.js
@@ -3,28 +3,28 @@ window.smartgeo_i18n_lang['en'] = {
         _AUTH_LOG_MESSAGE_REMOTE_           : "Remote login",
         _AUTH_LOG_MESSAGE_LOCAL_            : "Local login",
         _AUTH_LOG_MESSAGE_CHECK_            : "Checking server",
-        _AUTH_FORGET_PASSWORD_              : "Forget password",
+        _AUTH_FORGET_PASSWORD_              : "Forget saved password",
         _AUTH_SETTINGS_                     : "Settings",
         _AUTH_CHANGE_GIMAP_URL_             : "Change remote server",
         _AUTH_REMEMBER_PASSWORD_            : "Remember password",
         _AUTH_INIT_WITHOUT_NETWORK_ERROR_   : "Offline mode is not available for %s.",
-        _AUTH_SERVER_UNREACHABLE            : "" ,
+        _AUTH_SERVER_UNREACHABLE            : "Unreachable server" ,
         _AUTH_PLEASE_WAIT                   : "Please wait ..." ,
-        _AUTH_REQUIRED_FIELD_EMPTY          : "" ,
+        _AUTH_REQUIRED_FIELD_EMPTY          : "Please fill user and password fields." ,
         _AUTH_SERVER_ERROR                  : "%s", // status
 
         _SMARTGEO_ONLINE                    : "Smartgeo is online" ,
         _SMARTGEO_OFFLINE                   : "Smartgeo is offline" ,
-        _SMARTGEO_ZERO_SITE_SELECTED        : "" ,
+        _SMARTGEO_ZERO_SITE_SELECTED        : "No site found" ,
 
         _G3ME_UNKNOWN_GEOMETRY              : "Unknown geometry (%s)", // geom
 
         _CONSULTATION_LOADING               : "Loading ..." ,
         _CONSULTATION_REPORT                : "Report" ,
         _CONSULTATION_LOCATE                : "Locate" ,
-        _CONSULTATION_GOTO                  : "Go to asset" ,
+        _CONSULTATION_GOTO                  : "Go to" ,
         _CONSULTATION_REPORT_ON_POSITION    : "Fill report on this position" ,
-        _CONSULTATION_GPS_FAIL              : "" ,
+        _CONSULTATION_GPS_FAIL              : "Error while retrieving GPS coordinates" ,
 
         _INSTALL_INSTALLING                 : "Installing " ,
         _INSTALL_IN_PROGRESS                : " in progress" ,
@@ -46,7 +46,7 @@ window.smartgeo_i18n_lang['en'] = {
         _REPORT_CANCEL                      : "Cancel" ,
         _REPORT_NEXT                        : "Next" ,
         _REPORT_PREVIOUS                    : "Previous" ,
-        _REPORT_REQUIRED_FIELDS             : "" ,
+        _REPORT_REQUIRED_FIELDS             : "Please, fill required fields" ,
         _REPORT_MEDIA                       : "Media" ,
         _REPORT_ADD_PICTURES                : "Add pictures" ,
         _REPORT_SAVE                        : "Save" ,
@@ -58,28 +58,28 @@ window.smartgeo_i18n_lang['en'] = {
         _SEARCH_CRITERIA                    : "Criteria" ,
         _SEARCH_ERASE_CRITERIA              : "Erase criteria" ,
         _SEARCH_SEARCH_IN_PROGRESS          : "Search in progress" ,
-        _SEARCH_SEARCH_NO_RESULT            : "" ,
-        _SEARCH_PICK_OBJECT_TYPE            : "" ,
+        _SEARCH_SEARCH_NO_RESULT            : "No result" ,
+        _SEARCH_PICK_OBJECT_TYPE            : "Please select an object type" ,
 
         _SITE_LOADING                       : "Loading ..." ,
         _SITE_UNINSTALL                     : "Uninstalling " ,
         _SITE_UPDATE                        : "Updating " ,
 
-        _SYNC_SYNC_EVERYTHING               : "Synchronize everything" ,
+        _SYNC_SYNC_EVERYTHING               : "Synchronize all" ,
         _SYNC_SYNC                          : "Synchronize" ,
         _SYNC_DELETE                        : "Delete" ,
-        _SYNC_EVERYTHING_IS_SYNCED          : "" ,
+        _SYNC_EVERYTHING_IS_SYNCED          : "All reports have been synchronized." ,
         _SYNC_UPDATE                        : "Update " ,
         _SYNC_UNINSTALL                     : "Uninstall " ,
         _SYNC_UNKNOWN_ERROR_                : "Unknown error while synchronizing reports",
 
-        _INTENT_ZERO_SITE_SELECTED          : "" ,
+        _INTENT_ZERO_SITE_SELECTED          : "No site found" ,
         _INTENT_OBJECT_NOT_FOUND            : "Object not found in database" ,
         _INTENT_AUTH_FAILED                 : "%s", // status
 
-        _MAP_ZERO_SITE_SELECTED             : "" ,
-        _MAP_ZERO_OBJECT_FOUND              : "" ,
+        _MAP_ZERO_SITE_SELECTED             : "No site found"" ,
+        _MAP_ZERO_OBJECT_FOUND              : "No asset in this area" ,
         _MAP_MY_POSITION_CONTROL            : "My position" ,
-        _MAP_GPS_FAIL                       : "" ,
+        _MAP_GPS_FAIL                       : "Error while retrieving GPS coordinates" ,
         _MAP_CONSULTATION_CONTROL           : "Consultation"
 };
