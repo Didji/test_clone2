ab13025628eac84d14dd58362a06640fa8db895e
gulian <gulian@gulian.fr>
2013-11-20 11:36:43 +0100

    rm compiler.jar

---
+++ docs/ngdocs/js/docs-setup.js
@@ -0,0 +1,22 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "smartgeomobile.Smartgeo",
+      "shortName": "Smartgeo",
+      "type": "service",
+      "keywords": "_big_screen_threshold add and api device gimap global http if is isrunningonbigscreen isrunningonlittlescreen localstorage method methods needed on php provides reset return serveur service set setgimapurl smartgeo smartgeomobile to true url width"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
