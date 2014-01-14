11a9ada805ed69d4379c3e8c1804374c6c0812bc
gulian <gulian@gulian.fr>
2014-01-14 15:46:33 +0100

    Report: panel heading should be clickable on whole width (closes #258)

--- javascripts/controllers/mapController.js
+++ javascripts/controllers/mapController.js
@@ -537,7 +537,7 @@ angular.module('smartgeomobile').controller('mapController', function ($scope, $
         G3ME.map.addControl(CONSULTATION_CONTROL);
     }
 
-     function stopConsultation() {
+    function stopConsultation() {
         $scope.consultationIsEnabled = false;
         if(CONSULTATION_CONTROL && CONSULTATION_CONTROL._map) {
             G3ME.map.removeControl(CONSULTATION_CONTROL);
