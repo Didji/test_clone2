09fe94a56df75929c57a39f2d7a3f4d0501010e6
gulian <gulian@gulian.fr>
2014-01-24 16:26:22 +0100

    General: changement de version (0.11.0)

--- javascripts/factories/Smartgeo.js
+++ javascripts/factories/Smartgeo.js
@@ -18,7 +18,7 @@ angular.module('smartgeomobile').factory('Smartgeo', function($http, $window, $r
          * @const
          * @description Smartgeo mobile version, displayed on home page
          */
-        _SMARTGEO_MOBILE_VERSION : "0.10.0",
+        _SMARTGEO_MOBILE_VERSION : "0.11.0",
 
         /**
          * @ngdoc property
