404e1e63c85b6617a9e647cd2ea344f95a253c77
gulian <gulian@gulian.fr>
2014-02-11 10:33:50 +0100

    Release: modification droits pour release veolia + template droit pour veolia

--- app/javascripts/smartgeo-bootstrap.js
+++ app/javascripts/smartgeo-bootstrap.js
@@ -1,13 +1,24 @@
 angular.module("smartgeobootstrap", []).run(function ($rootScope) {
     (window.indexedDB ? window.smartgeoPersistenceIndexedDB : window.smartgeoPersistenceSQLite).get('sites', function(sites){
         window.smartgeoRightsManager = {
-            'report'                : true,
-            'goto'                  : true,
-            'planning'              : true,
+            'report'                : false,
+            'goto'                  : false,
+            'planning'              : false,
             'media'                 : true,
-            'logout'                : true,
-            '_DONT_REALLY_RESET'    : false
+            'logout'                : false,
+            '_DONT_REALLY_RESET'    : true
         };
+
+        // Right template for veolia
+        // window.smartgeoRightsManager = {
+        //     'report'                : false,
+        //     'goto'                  : false,
+        //     'planning'              : false,
+        //     'media'                 : true,
+        //     'logout'                : false,
+        //     '_DONT_REALLY_RESET'    : true
+        // };
+
         window.smartgeoPersistenceCache  = {};
         window.smartgeoPersistenceCache_ = {
             sites : sites
