3fd5c730fc5552ca90faa39fabc64a608a6b1ac4
gulian <gulian@gulian.fr>
2014-02-21 11:40:32 +0100

    Census: working on

--- app/javascripts/controllers/censusController.js
+++ app/javascripts/controllers/censusController.js
@@ -4,4 +4,11 @@ angular.module('smartgeomobile').controller('censusController', function ($scope
 
     $scope.map  = G3ME.map ;
 
+    $scope.startCensus = function(okey){
+        $scope.okey = okey ;
+    };
+    $scope.cancelCensus = function(){
+        $scope.okey = undefined ;
+    };
+
 });
