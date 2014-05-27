7c73fced2dd665f77c08f301ace0309a25339e47
gulian <gulian@gulian.fr>
2014-05-27 15:55:37 +0200

    Android: Désactivation de l'authentification en attendant la fonctionnalité côté proxy Gimap

--- app/javascripts/factories/Smartgeo.js
+++ app/javascripts/factories/Smartgeo.js
@@ -891,11 +891,12 @@ angular.module('smartgeomobile').factory('Smartgeo', function ($http, $window, $
 
             var EXTERNAL_TILEURL = Smartgeo.get_('sites')[site].EXTERNAL_TILEURL ;
 
-            if(window.SmartgeoChromium && (!EXTERNAL_TILEURL || ( EXTERNAL_TILEURL && Smartgeo.get('url').indexOf(EXTERNAL_TILEURL) !== -1))) {
-                var user = Smartgeo.get('user') ;
-                ChromiumCallbacks[16] = function(response){console.log(JSON.stringify(response));};
-                SmartgeoChromium.authenticate(Smartgeo.getServiceUrl('global.auth.json'), user.username, user.password, site);
-            }
+            // TODO : réactiver l'authentification dès que le proxy Gimap retourne une URL externe provenant du mapfile (ou non) (@gulian)
+            // if(window.SmartgeoChromium && (!EXTERNAL_TILEURL || ( EXTERNAL_TILEURL && Smartgeo.get('url').indexOf(EXTERNAL_TILEURL) !== -1))) {
+            //     var user = Smartgeo.get('user') ;
+            //     ChromiumCallbacks[16] = function(response){console.log(JSON.stringify(response));};
+            //     SmartgeoChromium.authenticate(Smartgeo.getServiceUrl('global.auth.json'), user.username, user.password, site);
+            // }
 
             if (!site) {
                 Smartgeo.log(("_SMARTGEO_ZERO_SITE_SELECTED"));
