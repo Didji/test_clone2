4d3d164f93145b131b7e4de7cacab12661c6f0e9
gulian <gulian@gulian.fr>
2014-09-23 11:19:15 +0200

    (Census: les listes déroulantes des fiches étaient vides (close #348))

---
+++ app/javascripts/filters/filter.listObjectToArray.js
@@ -0,0 +1,38 @@
+(function() {
+
+    'use strict';
+
+    angular
+        .module('smartgeomobile')
+        .filter('listObjectToArray', listObjectToArray);
+
+    listObjectToArray.$inject = ["Site"];
+
+    function listObjectToArray(Site) {
+        /**
+         * @name listObjectToArray_
+         * @desc
+         */
+
+        var lists = Site.current().lists ;
+        var listsArray = {};
+
+        function listObjectToArray_(options) {
+            if(listsArray[options]){
+                return listsArray[options];
+            }
+            var currentList = lists[options] ;
+            var out = [];
+            for (var value in currentList) {
+                out.push({
+                    'value': value,
+                    'label': currentList[value]
+                });
+            }
+            listsArray[options] =  out ;
+            return out;
+        }
+        return listObjectToArray_;
+    }
+
+})();
