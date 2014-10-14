406bbc1e58936141cac6bfa5e726862e7c9beb1a
gulian <gulian@gulian.fr>
2014-10-14 16:08:56 +0200

    Android: amélioration de la compatibilité avec Android KitKat 4.4

--- platforms/android/content-shell/src/com/gismartware/mobile/GimapMobileApplication.java
+++ platforms/android/content-shell/src/com/gismartware/mobile/GimapMobileApplication.java
@@ -27,26 +27,26 @@ import android.util.Log;
 public class GimapMobileApplication extends Application {
 	private static final String[] MANDATORY_PAK_FILES = new String[] {"content_shell.pak"};
     private static final String PRIVATE_DATA_DIRECTORY_SUFFIX = "content_shell";
-	
+
 	/**
 	 * Racine de l'application Web sur le terminal.
 	 */
 	public static String WEB_ROOT;
-	
+
 	/**
 	 * Chemin vers la page de lancement de l'application.
 	 */
 	public static String DEFAULT_URL;
-	
+
 	public static File EXT_APP_DIR;
-	
+
 	/**
 	 * Le contexte de l'application.
 	 */
 	public static Context context;
 
 	private static final String TAG = "GimapMobileApplication";
-	
+
 	private GimapMobileMainActivity mCurrentActivity = null;
 
     @Override
@@ -59,49 +59,56 @@ public class GimapMobileApplication extends Application {
         ResourceBundle config = ResourceBundle.getBundle("com.gismartware.mobile.config");
         WEB_ROOT = getCacheDir().getAbsolutePath() + File.separator + config.getString("application.directory");
 		DEFAULT_URL = WEB_ROOT + File.separator + config.getString("application.page.default");
-		
+
 		//initialise le chemin vers les tuiles stock�es sur la carte SD externe si elle existe, interne sinon
-		File extSdCardSlot = new File(getExtSdCard());
-		if (extSdCardSlot.canWrite()) { //carte SD "mont�e" si on peut �crire ou lire dedans..
-			Log.d(TAG, "External SD Card detected @ " + extSdCardSlot.getPath() + "...");
-			//TODO: g�rer plusieurs cartes SD
-			EXT_APP_DIR = new File(extSdCardSlot, "Android/data/com.gismartware.mobile/");
-			if (!EXT_APP_DIR.exists()) {
-				EXT_APP_DIR.mkdirs();
-			}
-		} else {
-			EXT_APP_DIR = context.getExternalFilesDir(null).getParentFile();
-			Log.d(TAG, "No external SD Card detected, use internal @ " + EXT_APP_DIR.getPath() + "...");
-		}
+        String sdCard  = getExtSdCard() ;
+        if(sdCard != null) {
+            File extSdCardSlot = new File(sdCard);
+            if (extSdCardSlot.canWrite()) { //carte SD "mont�e" si on peut �crire ou lire dedans..
+                Log.d(TAG, "External SD Card detected @ " + extSdCardSlot.getPath() + "...");
+                //TODO: g�rer plusieurs cartes SD
+                EXT_APP_DIR = new File(extSdCardSlot, "Android/data/com.gismartware.mobile/");
+                if (!EXT_APP_DIR.exists()) {
+                    EXT_APP_DIR.mkdirs();
+                }
+                return ;
+            }
+        }
+
+		EXT_APP_DIR = context.getExternalFilesDir(null).getParentFile();
+		Log.d(TAG, "No external SD Card detected, use internal @ " + EXT_APP_DIR.getPath() + "...");
     }
 
     public static void initializeApplicationParameters() {
         ResourceExtractor.setMandatoryPaksToExtract(MANDATORY_PAK_FILES);
         PathUtils.setPrivateDataDirectorySuffix(PRIVATE_DATA_DIRECTORY_SUFFIX);
     }
-    
+
     public GimapMobileMainActivity getCurrentActivity( ){
         return mCurrentActivity;
 	}
-  
+
     public void setCurrentActivity(GimapMobileMainActivity mCurrentActivity) {
     	this.mCurrentActivity = mCurrentActivity;
     }
-    
+
     private String getExtSdCard() {
-    	File file = new File("/system/etc/vold.fstab");
-        FileReader fr = null;
-        BufferedReader br = null;
-        
+
         String path = null;
+
         try {
+
+            File file = new File("/system/etc/vold.fstab");
+            FileReader fr = null;
+            BufferedReader br = null;
+
             fr = new FileReader(file);
             if (fr != null) {
                 br = new BufferedReader(fr);
                 String s = br.readLine();
                 while (s != null) {
                     if (s.startsWith("dev_mount")) {
-                        String[] tokens = s.split("\\s");                        
+                        String[] tokens = s.split("\\s");
                         if (!Environment.getExternalStorageDirectory().getAbsolutePath().equals(tokens[2])) {
                         	path = tokens[2];
                         	break;
@@ -109,17 +116,19 @@ public class GimapMobileApplication extends Application {
                     }
                     s = br.readLine();
                 }
-            } 
+            }
             if (fr != null) {
                 fr.close();
-            }            
+            }
             if (br != null) {
                 br.close();
             }
         } catch (FileNotFoundException e) {
         	Log.e(TAG, e.getMessage(), e);
         } catch (IOException e) {
-        	Log.e(TAG, e.getMessage(), e);
+            Log.e(TAG, e.getMessage(), e);
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage(), e);
         }
         return path;
     }
