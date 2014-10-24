21c581223a92b9275b0c24924f8df97d2935af49
gulian <gulian@gulian.fr>
2014-10-24 09:42:07 +0200

    (Veolia: mise à jour de l'intent controller)

--- app/javascripts/controllers/intentController.js
+++ app/javascripts/controllers/intentController.js
@@ -1,116 +1,145 @@
-angular.module('smartgeomobile').controller('intentController', function ($scope, $routeParams, $location, $rootScope, Smartgeo, $http, $window, G3ME, i18n, Icon, Storage, Site) {
+(function () {
 
     'use strict';
 
-    $scope.controller = $routeParams.controller;
+    angular
+        .module('smartgeomobile')
+        .controller('IntentController', IntentController);
 
+    IntentController.$inject = ["$scope", "$routeParams", "$location", "$rootScope", "Smartgeo", "$http", "$window", "G3ME", "i18n", "Icon", "Storage", "Site"];
 
-    function tokenAuth(token, callback) {
+    /**
+     * @class IntentController
+     * @desc Controlleur du menu de gestion des intents
+     */
+    function IntentController($scope, $routeParams, $location, $rootScope, Smartgeo, $http, $window, G3ME, i18n, Icon, Storage, Site) {
 
-        var currentUser = (Storage.get('users') || {})[Storage.get('lastUser')] || {};
-        currentUser.token = token;
-        Storage.set('user', currentUser);
+        var vm = this;
 
-        Smartgeo.login(token, callback, function (response) {
-            if ((response && response.status === 200) || !response) {
-                callback();
+        activate();
+
+        /**
+         * @name activate
+         * @desc Fonction d'initialisation
+         */
+        function activate() {
+            vm.controller = $routeParams.controller;
+
+            if (vm.controller === "oauth") {
+                if (!Storage.get("url") || Storage.get("url").indexOf($routeParams.url) === -1) {
+                    Smartgeo.setGimapUrl($routeParams.url);
+                }
+                return tokenAuth($routeParams.token, function () {
+                    $location.path('sites/');
+                });
+            }
+
+            if (!vm.controller) {
+                return false;
+            }
+
+            if ($routeParams.site) {
+                Site.current = Site.current || Storage.get_('sites')[$routeParams.site];
             } else {
-                alertify.alert(i18n.get("_INTENT_AUTH_FAILED", status));
-                $location.path('#');
+                var sites = Storage.get_('sites');
+                for (var siteId in sites) {
+                    if (sites.hasOwnProperty(siteId)) {
+                        Site.current = sites[siteId];
+                        break;
+                    }
+                }
             }
-        });
-    }
 
-    function redirect() {
-        switch ($scope.controller) {
-        case 'map':
-            $location.path('map/' + Site.current.id);
-            if (!$scope.$$phase) {
-                $scope.$apply();
+            if (!Site.current) {
+                alertify.alert(i18n.get("_INTENT_ZERO_SITE_SELECTED"));
+                $location.path("#");
+                return false;
             }
-            break;
 
-        case 'report':
-            $location.path('report/' + Site.current.id + '/' + $rootScope.report_activity + '/' + $rootScope.target + '/');
-            if (!$scope.$$phase) {
-                $scope.$apply();
+            for (var arg in $routeParams) {
+                if ($routeParams.hasOwnProperty(arg) && arg !== "controller" && arg !== "token") {
+                    $rootScope[arg] = $routeParams[arg];
+                }
             }
-            break;
+
+            if ($rootScope.map_target) {
+                // TODO: OULALA IT'S UGLY /!\ REFACTOR ALERT /!\
+                G3ME.parseTarget(Site.current, $rootScope.map_target, function (assets) {
+                    if (!assets.length) {
+                        alertify.alert(i18n.get("_INTENT_OBJECT_NOT_FOUND"));
+                        return tokenAuth($routeParams.token, redirect);
+                    }
+                    $rootScope.map_target = assets;
+                    if ($rootScope.map_marker === 'true' || $rootScope.map_marker === true) {
+                        $rootScope.map_marker = L.marker($rootScope.map_target, {
+                            icon: Icon.get('CONSULTATION')
+                        });
+                        if ($rootScope.report_target && $rootScope.report_activity) {
+                            $rootScope.map_marker.on('click', function () {
+                                $location.path('/report/' + Site.current.id + "/" + $rootScope.report_activity + "/" + $rootScope.report_target);
+                                $scope.$apply();
+                                if (!$scope.$$phase) {
+                                    $scope.$apply();
+                                }
+                            });
+                        }
+                    } else {
+                        $rootScope.map_marker = undefined;
+                    }
+                    tokenAuth($routeParams.token, redirect);
+                }, function () {
+                    alertify.alert(i18n.get("_INTENT_TARGET_NOT_VALID", $rootScope.map_target));
+                    $location.path('/map/');
+
+                });
+            } else {
+                tokenAuth($routeParams.token, redirect);
+            }
         }
-    }
 
-    if ($scope.controller === "oauth") {
-        if (!Storage.get("url") || Storage.get("url").indexOf($routeParams.url) === -1) {
-            Smartgeo.setGimapUrl($routeParams.url);
+        /**
+         * @name tokenAuth
+         * @desc Fonction d'authentification par token
+         */
+        function tokenAuth(token, callback) {
+            var currentUser = (Storage.get('users') || {})[Storage.get('lastUser')] || {};
+            currentUser.token = token;
+            Storage.set('user', currentUser);
+
+            Smartgeo.login(token, callback, function (response) {
+                if ((response && response.status === 200) || !response) {
+                    callback();
+                } else {
+                    alertify.alert(i18n.get("_INTENT_AUTH_FAILED", status));
+                    $location.path('/');
+                }
+            });
         }
-        return tokenAuth($routeParams.token, function () {
-            $location.path('sites/');
-        });
-    }
 
-    if (!$scope.controller) {
-        return false;
-    }
+        /**
+         * @name redirect
+         * @desc Fonction de redirection
+         */
+        function redirect() {
+            switch ($scope.controller) {
+            case 'map':
+                $location.path('map/' + Site.current.id);
+                if (!$scope.$$phase) {
+                    $scope.$apply();
+                }
+                break;
 
-    if ($routeParams.site) {
-        Site.current = Site.current || Storage.get_('sites')[$routeParams.site];
-    } else {
-        var sites = Storage.get_('sites');
-        for (var siteId in sites) {
-            if (sites.hasOwnProperty(siteId)) {
-                Site.current = sites[siteId];
+            case 'report':
+                $location.path('report/' + Site.current.id + '/' + $rootScope.report_activity + '/' + $rootScope.target + '/');
+                if (!$scope.$$phase) {
+                    $scope.$apply();
+                }
                 break;
             }
         }
+
+
+
     }
 
-    if (!Site.current) {
-        alertify.alert(i18n.get("_INTENT_ZERO_SITE_SELECTED"));
-        $location.path("#");
-        return false;
-    }
-
-    for (var arg in $routeParams) {
-        if ($routeParams.hasOwnProperty(arg) && arg !== "controller" && arg !== "token") {
-            $rootScope[arg] = $routeParams[arg];
-        }
-    }
-
-
-
-    if ($rootScope.map_target) {
-        // TODO: OULALA IT'S UGLY /!\ REFACTOR ALERT /!\
-        G3ME.parseTarget(Site.current, $rootScope.map_target, function (assets) {
-            if (!assets.length) {
-                alertify.alert(i18n.get("_INTENT_OBJECT_NOT_FOUND"));
-                return tokenAuth($routeParams.token, redirect);
-            }
-            $rootScope.map_target = assets;
-            if ($rootScope.map_marker === 'true' || $rootScope.map_marker === true) {
-                $rootScope.map_marker = L.marker($rootScope.map_target, {
-                    icon: Icon.get('CONSULTATION')
-                });
-                if ($rootScope.report_target && $rootScope.report_activity) {
-                    $rootScope.map_marker.on('click', function () {
-                        $location.path('/report/' + Site.current.id + "/" + $rootScope.report_activity + "/" + $rootScope.report_target);
-                        $scope.$apply();
-                        if (!$scope.$$phase) {
-                            $scope.$apply();
-                        }
-                    });
-                }
-            } else {
-                $rootScope.map_marker = undefined;
-            }
-            tokenAuth($routeParams.token, redirect);
-        }, function () {
-            alertify.alert(i18n.get("_INTENT_TARGET_NOT_VALID", $rootScope.map_target));
-            $location.path('/map/');
-
-        });
-    } else {
-        tokenAuth($routeParams.token, redirect);
-    }
-
-
-});
+})();
