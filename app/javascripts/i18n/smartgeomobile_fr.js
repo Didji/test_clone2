453081d0eb9f9d12b57257e47d2e343f4a250216
gulian <gulian@gulian.fr>
2014-10-31 10:06:18 +0100

    (Sonar compliance: duplication de code)

--- app/javascripts/i18n/smartgeomobile_fr.js
+++ app/javascripts/i18n/smartgeomobile_fr.js
@@ -1,251 +1,127 @@
 window.smartgeo_i18n_lang = window.smartgeo_i18n_lang || {};
 window.smartgeo_i18n_lang.fr = {
-
     _MENU_PARAMETERS: "Paramètres",
-
     _AUTH_LOG_MESSAGE_INIT_: "Initialisation",
-
     _AUTH_LOG_MESSAGE_REMOTE_: "Connexion distante",
-
     _AUTH_LOG_MESSAGE_LOCAL_: "Connexion locale",
-
     _AUTH_LOG_MESSAGE_CHECK_: "Vérification du serveur",
-
     _AUTH_LOG_MESSAGE_LOGIN_: "Connexion",
-
     _AUTH_FORGET_PASSWORD_: "Oublier le mot de passe enregistré",
-
     _AUTH_SETTINGS_: "Paramètres",
-
     _AUTH_CHANGE_GIMAP_URL_: "Changer de serveur",
-
     _AUTH_REMEMBER_PASSWORD_: "Se souvenir du mot de passe",
-
     _AUTH_INIT_WITHOUT_NETWORK_ERROR_: "Le mode déconnecté n'est pas disponible pour %s car il ne s'est jamais authentifié en mode connecté.",
-
     _AUTH_SERVER_UNREACHABLE: "Serveur injoignable",
-
     _AUTH_PLEASE_WAIT: "Veuillez patienter ...",
-
     _AUTH_REQUIRED_FIELD_EMPTY: "Veuillez renseigner les champs utilisateur et mot de passe.",
-
     _AUTH_SERVER_ERROR: "Une erreur est survenue (%s)",
-
     _AUTH_INCORRECT_PASSWORD: "Authentification incorrecte",
-
     _AUTH_ERROR_: "Connexion impossible",
-
     _AUTH_UNKNOWN_ERROR_OCCURED_: "Connexion impossible",
-
     _REGISTER_REGISTER_: "Enregister",
-
     _REGISTER_ERROR_OFFLINE: "Vous devez être connecté à Internet pour vous enregister.",
-
     _REGISTER_ERROR_NOT_FOUND: "Le numéro de licence fourni n'existe pas.",
-
     _REGISTER_ERROR_CONFLICT: "Cette licence a déjà été enregistée.",
-
     _REGISTER_ERROR_G3LIC: "Une erreur s'est produite, veuillez contacter votre support.",
-
     _REGISTER_ERROR_UNKNOWN: "Une erreur s'est produite, veuillez contacter votre support.",
-
     _REGISTER_ERROR_NO_MORE_LICENSE: "Le nombre d'utilisation maximal de cette licence a été atteint.",
-
     _REGISTER_LICENSE_REVOKED: "La license n'est plus valide.",
-
     _REGISTER_MUST_CHECK_: "Limite d'utilisation hors ligne atteinte, veuillez vous connecter à Internet.",
-
     _REGISTER_CAREFUL_: "Attention, vous avez bientôt atteint le nombre maximal d'utilisation en mode déconnecté (%s restante(s))",
-
     _REGISTER_UPDATE_LICENSE_: "Vérifier la licence",
-
     _REGISTER_RECORD_ID_: "Votre licence est valide. Veuillez noter l'identifiant suivant qui vous sera demandé en cas de perte ou vol de ce terminal : ",
-
     _CONFIRM_CHANGE_SITE_: "Voulez-vous vraiment changer de site&nbsp?",
-
     _CONFIRM_DISCONNECT_: "Voulez-vous vraiment quitter l'application&nbsp?",
-
     _G3ME_UNKNOWN_GEOMETRY: "Géometrie inconnue (%s)",
-
     _SMARTGEO_ONLINE: "Smartgeo est connecté",
-
     _SMARTGEO_OFFLINE: "Smartgeo est déconnecté",
-
     _SMARTGEO_ZERO_SITE_SELECTED: "Aucun site trouvé",
-
     _CONSULTATION_LOADING: "Chargement",
-
     _CONSULTATION_REPORT: "Compte rendu",
-
     _CONSULTATION_LOCATE: "Localiser",
-
     _CONSULTATION_GOTO: "S'y rendre",
-
     _CONSULTATION_REPORT_ON_POSITION: "Compte rendu sur cette position",
-
     _CONSULTATION_GPS_FAIL: "Erreur lors de la récupération des coordonnées GPS",
-
     _CONSULTATION_ADD_TO_: "Ajouter à ",
-
     _CONSULTATION_WRITE_FOR_: "Saisir pour ",
-
     _CONSULTATION_ADD_TO_SELECTION: "Ajouter à la selection",
-
     _INSTALL_INSTALLING: "Installation de ",
-
     _INSTALL_IN_PROGRESS: "en cours",
-
     _UNINSTALL_UNINSTALLING: "Désinstallation de ",
-
     _UPDATE_UPDATING: "Mise à jour de ",
-
     _MENU_CONSULTATION: "Consultation",
-
     _MENU_PLANNING: "Planning",
-
     _MENU_SEARCH: "Recherche",
-
     _MENU_MY_POSITION: "Ma position",
-
     _MENU_ACTIVE_LAYERS: "Couches",
-
     _MENU_BACK: "Retour",
-
     _MENU_REPORT: "Compte rendu",
-
     _MENU_SYNC: "Synchronisation",
-
     _MENU_CENSUS: "Recensement",
-
     _MENU_LOGOUT: "Déconnexion",
-
     _MENU_CHANGE_SITE: "Changer de site",
-
     _REPORT_REPORT: "Compte rendu",
-
-    _REPORT_CANCEL: "Annuler",
-
     _REPORT_NEXT: "Suivant",
-
     _REPORT_PREVIOUS: "Précedent",
-
-    _REPORT_REQUIRED_FIELDS: "Certains champs obligatoires ne sont pas renseignés.",
-
-    _REPORT_MEDIA: "Média",
-
-    _REPORT_ADD_PICTURES: "Ajouter une photo",
-
-    _REPORT_SAVE: "Enregistrer",
-
-    _REPORT_LOADING: "Chargement",
-
     _REPORT_DELETE: "Supprimer",
-
+    _REPORT_MEDIA: "Média",
     _REPORT_SELECT_ACTIVITY: "Sélectionner activité",
-
+    _REPORT_REQUIRED_FIELDS: "Certains champs obligatoires ne sont pas renseignés.",
+    _REPORT_ADD_PICTURES: "Ajouter une photo",
+    _REPORT_LOADING: "Chargement",
+    _REPORT_CANCEL: "Annuler",
+    _REPORT_SAVE: "Enregistrer",
     _REPORT_SELECT_ASSETS: "Sélectionner patrimoine",
-
     _SEARCH_SEARCH: "Recherche",
-
-    _SEARCH_ADVANCED_SEARCH: "Recherche avancée",
-
     _SEARCH_BACK: "Retour",
-
+    _SEARCH_ADVANCED_SEARCH: "Recherche avancée",
     _SEARCH_TYPE: "Type",
-
     _SEARCH_CRITERIA: "Critères",
-
-    _SEARCH_ERASE_CRITERIA: "Supprimer les critères",
-
-    _SEARCH_SEARCH_IN_PROGRESS: "Recherche en cours",
-
-    _SEARCH_SEARCH_NO_RESULT: "Aucun résultat",
-
-    _SEARCH_PICK_OBJECT_TYPE: "Veuillez sélectionner un type d'objet",
-
-    _SEARCH_SEARCH_HAS_BEEN_CANCELED: "La recherche a été annulée",
-
     _SEARCH_CANCEL_SEARCH: "Annuler",
-
-    _SITE_LOADING: "Chargement",
-
+    _SEARCH_SEARCH_IN_PROGRESS: "Recherche en cours",
+    _SEARCH_SEARCH_NO_RESULT: "Aucun résultat",
+    _SEARCH_PICK_OBJECT_TYPE: "Veuillez sélectionner un type d'objet",
+    _SEARCH_ERASE_CRITERIA: "Supprimer les critères",
+    _SEARCH_SEARCH_HAS_BEEN_CANCELED: "La recherche a été annulée",
     _SITE_UNINSTALL: "Désinstallation de ",
-
     _SITE_UPDATE: "Mise à jour de ",
-
+    _SITE_LOADING: "Chargement",
     _SYNC_SYNC_EVERYTHING: "Synchroniser",
-
-    _SYNC_SYNC_IN_PROGRESS: "Synchronisation en cours",
-
     _SYNC_SYNC: "Synchroniser",
-
+    _SYNC_SYNC_IN_PROGRESS: "Synchronisation en cours",
     _SYNC_DELETE: "Supprimer",
-
     _SYNC_EVERYTHING_IS_SYNCED: "Rien à synchroniser",
-
     _SYNC_UPDATE: "Mettre à jour les données ",
-
     _SYNC_UNINSTALL: "Désinstaller",
-
     _SYNC_UNKNOWN_ERROR_: "Erreur inconnue lors de la synchronisation.",
-
     _SYNC_UNINSTALL_CONFIRM_MESSAGE_: "Voulez vous désinstaller %s&nbsp?",
-
     _SYNC_UPDATE_CONFIRM_MESSAGE_: "Voulez vous mettre à jour %s&nbsp?",
-
     _INTENT_ZERO_SITE_SELECTED: "Aucun site trouvé",
-
     _INTENT_OBJECT_NOT_FOUND: "L'objet n'a pas été trouvé dans la base de données du terminal",
-
     _INTENT_AUTH_FAILED: "L'authentification a échoué (%s)",
-
     _MAP_ZERO_SITE_SELECTED: "Aucun site trouvé",
-
     _MAP_ZERO_OBJECT_FOUND: "Aucun patrimoine dans cette zone",
-
     _MAP_MY_POSITION_CONTROL: "Ma position",
-
     _MAP_GPS_FAIL: "Erreur lors de la récupération des coordonnées GPS",
-
     _MAP_CONSULTATION_CONTROL: "Consultation",
-
     _MAP_REFERENCE_VIEW_CONTROL: "Vue de référence",
-
     _PLANNING_SYNC_FAIL_: "Erreur lors de la mise à jour des missions",
-
     _PLANNING_OBJECT_NOT_FOUND_: "Les objets de cette mission n'ont pas été trouvés.",
-
     _PLANNING_REFRESH_: "Actualiser",
-
     _PLANNING_START_: "Démarrer",
-
     _PLANNING_TODAY_: "Aujourd'hui",
-
     _PLANNING_BEFORE_: "Avant",
-
     _PLANNING_DISPLAY_DONE_ASSETS_: "Afficher objets terminés",
-
     _PLANNING_LOCATE_: "Localiser",
-
     _PLANNING_WRITE_: "CR",
-
     _PLANNING_FINISH_: "terminé",
-
     _PLANNING_REFRESHED_: "Actualisé il y a ",
-
     _PLANNING_DISPLAY_FINISHED_: "Afficher les OT terminés",
-
     _PLANNING_NEW_MISSION_: " nouvelle mission",
-
     _PLANNING_NEW_MISSIONS_: " nouvelles missions",
-
     _CENSUS_INFORMATION_: " Recensement",
-
     _CENSUS_CHOOSE_OBJECT_: "Veuillez sélectionner l'objet à recenser",
-
     _NIGHTTOUR_STOP_: "Arrêter",
-
     _NIGHTTOUR_CLOSE_: "Clôturer",
-
     _NIGHTTOUR_SAVING_: "Sauvegarde en cours"
 };
