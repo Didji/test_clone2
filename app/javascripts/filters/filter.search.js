a8c198baefb8c52c7ded5d8a8ff76fec8915f5b3
gulian <gulian@gulian.fr>
2015-03-30 16:02:50 +0200

    (Recherche: Listes dans la recherche triées close #548)

---
+++ app/javascripts/filters/filter.search.js
@@ -0,0 +1,35 @@
+(function() {
+
+    'use strict';
+
+    angular
+        .module( 'smartgeomobile' )
+        .filter( 'orderByObjects', orderByObjects );
+
+    function orderByObjects() {
+        /**
+         * @name _orderByObjects
+         * @desc
+         */
+
+
+        var filtered = [];
+
+        function _orderByObjects(items) {
+            console.log( ' ' );
+            filtered = [];
+            angular.forEach( items, function(item, i) {
+                filtered.push( {
+                    value: i,
+                    label: item
+                } );
+            } );
+            filtered.sort( function(a, b) {
+                return (a.label.toLowerCase() > b.label.toLowerCase() ? 1 : -1);
+            } );
+            return filtered;
+        }
+        return _orderByObjects;
+    }
+
+})();
