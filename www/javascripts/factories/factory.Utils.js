ed18791bacc34ef35156a03d705824d61fc03359
Tibneyret <thibaud.neyret@gmail.com>
2015-11-05 10:45:47 +0100

    fix #735 reset de G3ME.map lors du Utils .clearPersistence , appelé a chaque authentification, et donc changement de site

--- www/javascripts/factories/factory.Utils.js
+++ www/javascripts/factories/factory.Utils.js
@@ -6,9 +6,9 @@
         .module( 'smartgeomobile' )
         .factory( 'Utils', UtilsFactory );
 
-    UtilsFactory.$inject = ["$http", "Storage", "$window", "SQLite", "$rootScope"];
+    UtilsFactory.$inject = ["$http", "Storage", "$window", "SQLite", "$rootScope", "G3ME"];
 
-    function UtilsFactory($http, Storage, $window, SQLite, $rootScope) {
+    function UtilsFactory($http, Storage, $window, SQLite, $rootScope, G3ME) {
 
         /**
          * @class UtilsFactory
@@ -34,6 +34,8 @@
             if (missions) {
                 Storage.set( 'missions_' + (Storage.get( 'users' ) || {})[Storage.get( 'lastUser' )].username, missions );
             }
+            //reset the extent map when change site
+            G3ME.map = null;
         };
 
         /**
