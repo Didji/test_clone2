5fedfd9146736a256b62c31af74c583f1874df5d
hahtreyou <marc.beudin@gmail.com>
2015-12-14 14:16:17 +0100

    (Correction erreur syntaxe suite merge)

--- www/javascripts/controllers/siteInstallController.js
+++ www/javascripts/controllers/siteInstallController.js
@@ -103,13 +103,13 @@ angular.module( 'smartgeomobile' ).controller( 'siteInstallController', ["$scope
                 });
             });
         });
+    });
 
-        $scope.$on( "_INSTALLER_I_AM_CURRENTLY_DOING_THIS_", function(event, action) {
-            $scope.currentInstalledOkey = action.okey;
-            stepsByOkey[action.okey].progress = 1 * action.progress;
-            if (!$scope.$$phase) {
-                $scope.$apply();
-            }
-        });
-    }
-] );
+    $scope.$on( "_INSTALLER_I_AM_CURRENTLY_DOING_THIS_", function(event, action) {
+        $scope.currentInstalledOkey = action.okey;
+        stepsByOkey[action.okey].progress = 1 * action.progress;
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
+    });
+}]);
