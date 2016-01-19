89ded95e5165af2b4d8fa437d0812d48282b7cb5
hahtreyou <marc.beudin@gmail.com>
2016-01-19 18:09:53 +0100

    (On filtre la recherche avancée pour ne pas afficher les okey projet)

---
+++ www/javascripts/filters/filter.object.js
@@ -0,0 +1,18 @@
+(function() {
+
+    'use strict';
+
+    angular
+        .module('smartgeomobile')
+        .filter('filterObj', function() {
+            return function(items, field, value) {
+                var filtered = [];
+                angular.forEach(items, function(item) {
+                    if (item[field] !== value) {
+                        filtered.push(item);
+                    }
+                });
+                return filtered;
+            };
+        });
+})();
