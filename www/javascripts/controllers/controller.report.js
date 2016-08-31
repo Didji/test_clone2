6d7684bbfc2797ee27c212aaa908edd887540df0
Jimmy TOUBERT <jtoubert@student.42.fr>
2016-08-31 14:59:47 +0200

    La réalisation d'un CR sur un objet depuis un intent retourne un JSON invalide (fix #989 bis bis)

--- www/javascripts/controllers/controller.report.js
+++ www/javascripts/controllers/controller.report.js
@@ -179,6 +179,9 @@
                 if (report.fields[i] && typeof report.fields[i] === "object" && report.fields[i].id && report.fields[i].text) {
                     report.fields[i] = report.fields[i].id;
                 }
+                if (report.fields[i] === "") {
+                    delete report.fields[i];
+                }
             }
             for (i = 0; i < report.ged.length; i++) {
                 report.ged[i] = {
@@ -291,7 +294,7 @@
                     }
                     vm.report.roFields[field.id] = output;
                     vm.report.overrides[field.id] = output;
-                    fields[field.id] = output.length != 0 ? output : def;
+                    fields[field.id] = output.length != 0 ? output : Object.keys(def).length != 0 ? def : output;
                 }
             }
             if (!$scope.$$phase) {
@@ -419,7 +422,9 @@
         function parsedReport() {
             var report = {};
             for (var i in vm.report.fields) {
-               report[vm.report.activity._fields[i].label] = (typeof vm.report.fields[i] == "object" && vm.report.fields[i].toString().match(/object/) != null && jQuery.isEmptyObject(vm.report.fields[i])) ? undefined : vm.report.fields[i];
+                if (typeof vm.report.fields[i] !== "object" && vm.report.fields[i].toString().match(/object/) == null && vm.report.fields[i] != '') {
+                    report[vm.report.activity._fields[i].label] = vm.report.fields[i];
+                }
             }
             return report;
         }
