e1a66a166d6c6e204ecddf64ea293302270f6acf
Thomas Nicolazzi <tnicolazzi@gismartware.com>
2018-06-28 16:23:00 +0200

    :bug: Intent - Correctif d'affichage des markers sur appel d'intent

--- www/javascripts/controllers/controller.intent.js
+++ www/javascripts/controllers/controller.intent.js
@@ -23,6 +23,8 @@
          */
         function activate() {
             intent = $routeParams;
+            // Si l'intent vient d'un RI sans asset, on masque le marker
+            intent.map_marker = intent.map_target.match(/^(\d+.?\d+),([-+]?\d+.?\d+)$/) ? false : true;
 
             Storage.set( 'intent', intent );
             if (!intent.controller) {
