c2b2f48375e238570cb558600ca4e0c1b851f51c
Thomas Nicolazzi <tnicolazzi@gismartware.com>
2018-08-30 09:12:00 +0200

    :wrench: Modification des templates de configuration

--- www/javascripts/config.template.js
+++ www/javascripts/config.template.js
@@ -1,29 +1,28 @@
-angular
-	.module( 'smartgeomobile.config', [] )
-	.constant( 'RIGHTS', {
-		"activelayers": true,
-		"census": true,
-		"consultation": true,
-		"downloadTiles": false,
-		"goto": true,
-		"history": true,
-		"logout": true,
-		"media": true,
-		"multiselection": true,
-		"myposition": true,
-		"oauth": false,
-		"onlyUpdateSiteDaily": false,
-		"parameters": true,
-		"photo": true,
-		"planning": true,
-		"project": true,
-		"report": true,
-		"search": true,
-		"serverUrl": "http://dev-canopee-frontal.hp.m-ve.com/",
-		"siteselection": true,
-		"synccenter": true,
-		"intent": 200,
-		"_DONT_REALLY_RESET": false,
-	});
-
-
+angular.module("smartgeomobile.config", []).constant("RIGHTS", {
+    activelayers: true,
+    census: true,
+    consultation: true,
+    downloadTiles: true,
+    goto: true,
+    history: true,
+    logout: true,
+    media: true,
+    multiselection: true,
+    myposition: true,
+    oauth: false,
+    onlyUpdateSiteDaily: true,
+    parameters: true,
+    photo: true,
+    planning: true,
+    project: true,
+    report: true,
+    search: true,
+    serverUrl: "http://dev-canopee-frontal.hp.m-ve.com/",
+    serverDomain: "@m-ve.com",
+    sketch: true,
+    siteselection: false,
+    synccenter: true,
+    intent: 200,
+    _MAX_SIZE_POST_REQ: 2500000, //2,5Mo
+    _DONT_REALLY_RESET: false
+});
